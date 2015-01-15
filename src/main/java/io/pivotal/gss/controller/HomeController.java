7c7b1900577193e63e135f29260ab202c848fc28
William Gu <sgu@pivotal.io>
2015-01-15 10:47:24 +0800

    Add base spring project

---
+++ src/main/java/io/pivotal/gss/controller/HomeController.java
@@ -0,0 +1,14 @@
+package io.pivotal.gss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String home() {
+		return "views/home.jsp";
+	}
+
+}
